a8cf673f8f2bb909dada9e6d1bd719dd250f9a27
zhu <blacknstones@gmail.com>
2021-01-16 02:16:20 +0100

    add control logic and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import styled from "styled-components";
 import produce from "immer";
 import preset from "./preset.json";
-// import Sketch from "react-p5";
 
 const rowsLength = 40;
 const colsLength = 20;
@@ -10,16 +9,18 @@ const colsLength = 20;
 const Cell = styled.div`
   width: calc(90vw / 40);
   height: calc(90vw / 40);
+  max-width: calc(1024px / 40);
+  max-height: calc(1024px / 40);
   background: ${(props) => (props.isAlive ? "white" : "black")};
   border: 1px solid transparent;
 `;
 
-const makeEmptyGrid = (): number[][] => {
+const generateGrid = (option: "empty" | "random"): number[][] => {
   let emptyGrid = [];
   for (let x = 0; x < rowsLength; x++) {
     emptyGrid[x] = [];
     for (let y = 0; y < colsLength; y++) {
-      emptyGrid[x][y] = 0;
+      emptyGrid[x][y] = option === "empty" ? 0 : Math.round(Math.random());
     }
   }
   return emptyGrid;
@@ -28,22 +29,84 @@ const makeEmptyGrid = (): number[][] => {
 const App: React.FC = () => {
   const [grid, setGrid] = useState(
     preset
+    // Uncomment below to set initial grid to empty
     //   () => {
-    //   return makeEmptyGrid();
+    //   return generateEmptyGrid();
     // }
   );
 
   const [isRunning, setIsRunning] = useState(false);
+  const runningRef = useRef(isRunning);
+  runningRef.current = isRunning;
 
-  // use immer to nutate cell and update grid state
+  const [interval, setInterval] = useState(200);
+
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  // use immer to mutate grid state
   const handleCellClick = (x: number, y: number) => {
+    // setGrid(grid => {
+    //   let newGrid = grid;
+    //   newGrid[x][y] = grid[x][y] ? 0 : 1;
+    //   return newGrid;
+    // });
+
     let newGrid = produce(grid, (draftGrid) => {
       draftGrid[x][y] = grid[x][y] ? 0 : 1;
     });
     setGrid(newGrid);
     console.log("clicked");
-    console.table(JSON.stringify(grid));
+    // console.table(JSON.stringify(grid));
   };
+
+  const countNeighbors = (grid: number[][], x: number, y: number): number => {
+    let count = 0;
+    // iterate neighbor cells
+    for (let i of [-1, 0, 1]) {
+      for (let j of [-1, 0, 1]) {
+        // ignore edge cells
+        if (
+          x + i >= 0 &&
+          x + i < rowsLength &&
+          y + j >= 0 &&
+          y + j < colsLength
+        ) {
+          count += grid[x + i][y + j];
+        }
+      }
+    }
+    // subtract the cell itself
+    count -= grid[x][y];
+    return count;
+  };
+
+  // useCallback hook to avoid creating a new function in every re-render
+  const runIteration = useCallback(() => {
+    console.log("isRunning: ", isRunning);
+    console.log("runningRef", runningRef);
+    // useRef hook to grab the current isRunning state
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((grid) =>
+      produce(grid, (draftGrid) => {
+        for (let i = 0; i < rowsLength; i++) {
+          for (let j = 0; j < colsLength; j++) {
+            let neighbors = countNeighbors(grid, i, j);
+            // rule for generating next cell state
+            if (neighbors < 2 || neighbors > 3) {
+              draftGrid[i][j] = 0;
+            } else if (grid[i][j] === 0 && neighbors === 3) {
+              draftGrid[i][j] = 1;
+            }
+          }
+        }
+      })
+    );
+
+    setTimeout(runIteration, interval);
+  }, [interval, isRunning]);
 
   return (
     <div className="App">
@@ -68,20 +131,49 @@ const App: React.FC = () => {
         </div>
 
         <div className="menu">
-
-          <button className="menu-button">
+          <button
+            className="menu-button"
+            onClick={() => {
+              setMenuIsOpen(!menuIsOpen);
+            }}
+          >
             <i className="far fa-circle"></i>
           </button>
 
-          <div className="toggle">
-            <p className="intro">Game of Life</p>
-            <div className="actions">
-              <button>Run</button>
-              <button>Reset</button>
-              <button>Random</button>
+          {menuIsOpen && (
+            <div className="toggle">
+              <p className="intro">Game of Life</p>
+              <div className="actions">
+                <button
+                  onClick={() => {
+                    setIsRunning(!isRunning);
+                    if (!isRunning) {
+                      runningRef.current = true;
+                      runIteration();
+                    }
+                  }}
+                >
+                  {isRunning ? "Stop" : "Run"}
+                </button>
+                <button
+                  onClick={() => {
+                    setIsRunning(false);
+                    setGrid(preset);
+                  }}
+                >
+                  Reset
+                </button>
+                <button
+                  onClick={() => {
+                    setIsRunning(false);
+                    setGrid(generateGrid("random"));
+                  }}
+                >
+                  Random
+                </button>
+              </div>
             </div>
-          </div>
-
+          )}
         </div>
       </div>
     </div>
